bf593592b5ed528631a3692256fad587facb47b3
naheem adedokun <naheemadedokun21@gmail.com>
2022-02-15 22:29:41 +0100

    project on reddit clone

---
+++ src/pages/_app.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+// import Header from "../components/Header";
+import theme from "../theme";
+import type { AppProps } from "next/app";
+// import Amplify from "aws-amplify";
+
+// import awsconfig from "../aws-exports";
+// import AuthContext from "../context/AuthContext";
+
+// Amplify.configure({ ...awsconfig, ssr: true });
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Reddit Clone</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      {/* <AuthContext> */}
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          {/* <Header /> */}
+          <Component {...pageProps} />
+        </ThemeProvider>
+      {/* </AuthContext> */}
+    </React.Fragment>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+// import Head from 'next/head'
+import Image from 'next/image'
+import {Typography} from '@mui/material'
+
+export default function Home() {
+  return (
+    <Typography variant="h1">Hello there</Typography>
+  )
+}
